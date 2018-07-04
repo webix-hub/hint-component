eb652d07ec7223d09b7424a79e79ca3ce101f9c6
Helga <okustova@xbsoftware.com>
2018-07-04 14:34:38 +0200

    [fix] step timeout can be configured

--- sources/hint.js
+++ sources/hint.js
@@ -11,7 +11,8 @@ webix.protoUI({
 		nextButton: true,
 		prevButton: true,
 		top: false,
-		left: false
+		left: false,
+		stepTimeout:500
 	},
 	$init() {
 		this.$view.className += " webix_hint_view";
@@ -55,7 +56,7 @@ webix.protoUI({
 			</defs>
 			<rect class="webix_hint_overlay_hole" width="100%" height="100%" mask="url(#hole)" />
 		</svg>`;
-		this._setProperties(stepEl);
+		this._setProperties(stepEl, false, 1);
 		this.callEvent("onAfterStart", []);
 	},
 	_drawHint() {
@@ -73,7 +74,7 @@ webix.protoUI({
 			<button class="webix_hint_button_close" title="Close">&#10005;</button>
 		</div>`;
 	},
-	_setProperties(stepEl, refresh) {
+	_setProperties(stepEl, refresh, timeout) {
 		if(!stepEl) {
 			return;
 		}
@@ -140,7 +141,7 @@ webix.protoUI({
 			this._hint.style.cssText = `top:${hintTop}px; left:${hintLeft}px;`;
 			this._setAttributes(this.$view.getElementsByClassName("webix_hint_overlay_hole_el")[0], {"x":elLeft-this._step.padding*2, "y":elTop-this._step.padding*2, "width":highlightWidth+this._step.padding *2, "height":highlightHeight+this._step.padding*2});
 			webix.html.addCss(this.getNode(), "webix_hint_animated");
-		}, 500);
+		}, timeout || this.config.stepTimeout);
 	},
 	_setPos(name) {
 		if(this._isInteger(this._step[name])) {
